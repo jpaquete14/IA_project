7af0e3b77ccc0430573c40f4d81649f04bc93d50
jpaquete14 <jpaquete14@gmail.com>
2019-12-03 11:37:24 +0000

    first version, not tested

--- IA1920Proj2alunosv01/ruagomesfreiregame2sol-template.py
+++ IA1920Proj2alunosv01/ruagomesfreiregame2sol-template.py
@@ -1,4 +1,10 @@
 import random
+import numpy
+
+#Globa Variables for Q table calculation
+alpha = 0,1
+gamma = 0,7
+epsilon = 0,7
 
 # LearningAgent to implement
 # no knowledeg about the environment can be used
@@ -13,6 +19,9 @@ class LearningAgent:
                 # define this function
                 self.nS = nS
                 self.nA = nA
+                self.q_table = np.zeros([nS, nA])
+
+
                 # define this function
               
         
@@ -25,8 +34,13 @@ class LearningAgent:
         def selectactiontolearn(self,st,aa):
                 # define this function
                 # print("select one action to learn better")
-
+                
                 a = 0
+                if random.uniform(0, 1) < epsilon:
+                        a = random.randint(0, len(aa) - 1) # Exploration
+                else:
+                        a = index_max(st, aa) # Exploitation
+
                 # define this function
                 return a
 
@@ -37,19 +51,36 @@ class LearningAgent:
         # returns
         # a - the index to the action in aa
         def selectactiontoexecute(self,st,aa):
+
                 # define this function
-                a = 0
+                a = index_max(st, aa)
+
                 # print("select one action to see if I learned")
                 return a
 
 
         # this function is called after every action
-        # st - original state
+        # ost - original state
         # nst - next state
         # a - the index to the action taken
         # r - reward obtained
         def learn(self,ost,nst,a,r):
                 # define this function
                 #print("learn something from this data")
+
+                old_q = self.q_table[ost, a]
+                next_max = np.max(self.q_table[nst])
+        
+                new_q = (1 - alpha) * old_q + alpha * (r + gamma * next_max)
+                self.q_table[ost, a] = new_q
                 
                 return
+
+        def index_max(self, lst):
+                max = float("-inf")
+                index_max = 0
+                for i in range (0, len(lst)):
+                        if self.q_table[st][i - 1] > max:
+                                max = self.q_table[st][i]
+                                index_max = i
+        
