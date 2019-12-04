5a50bd3cbc85f4ee95f041456de3f9df5e12ede1
jpaquete14 <jpaquete14@gmail.com>
2019-12-04 00:23:42 +0000

    working version

--- IA1920Proj2alunosv01/ruagomesfreiregame2sol.py
+++ IA1920Proj2alunosv01/ruagomesfreiregame2sol.py
@@ -1,4 +1,10 @@
 import random
+import numpy as np
+
+#Globa Variables for Q table calculation
+alpha = 0.1
+gamma = 0.5
+epsilon = 0.5
 
 # LearningAgent to implement
 # no knowledeg about the environment can be used
@@ -10,12 +16,17 @@ class LearningAgent:
         # nA maximum number of action per state
         def __init__(self,nS,nA):
 
-                # define this function
                 self.nS = nS
                 self.nA = nA
-                # define this function
-              
+                
+                #Q_TABLE inicialization
+                self.q_table = []
+                for i in range (0, nS):
+                        #list for each state that contains the possible actions
+                        self.q_table.append([]) 
+                #######################
         
+
         # Select one action, used when learning  
         # st - is the current state        
         # aa - is the set of possible actions
@@ -23,11 +34,32 @@ class LearningAgent:
         # returns
         # a - the index to the action in aa
         def selectactiontolearn(self,st,aa):
-                # define this function
                 # print("select one action to learn better")
 
+                def index_max(lst):
+                        max = float("-inf")
+                        index_max = 0
+                        for el in self.q_table[st]:
+                                #check highest q value
+                                for i in range (0, len(aa)):
+                                        if self.q_table[st][i] > max:
+                                                max = self.q_table[st][i]
+                                                index_max = i
+                        return index_max
+
+                #action list initialization
+                if not self.q_table[st]:       
+                        for i in range (0, len(aa)):
+                                self.q_table[st].append(0)
+
+
                 a = 0
-                # define this function
+                if random.uniform(0, 1) < epsilon:      # Exploration
+                        a = random.randint(0, len(aa) - 1) 
+
+                else:   # Exploitation
+                        a = index_max(aa)
+
                 return a
 
         # Select one action, used when evaluating
@@ -37,19 +69,41 @@ class LearningAgent:
         # returns
         # a - the index to the action in aa
         def selectactiontoexecute(self,st,aa):
-                # define this function
-                a = 0
+
+                def index_max(lst):
+                        max = float("-inf")
+                        index_max = 0
+                        for i in range (0, len(lst)):
+                                if self.q_table[st][i] > max:
+                                        max = self.q_table[st][i]
+                                        index_max = i
+                        return index_max
+
+                 #action list initialization (Dont know if needed)
+                if not self.q_table[st]:       
+                        for i in range (0, len(aa)):
+                                q_table[st].append(0)
+
+                a = index_max(aa)
+
                 # print("select one action to see if I learned")
                 return a
 
 
         # this function is called after every action
-        # st - original state
+        # ost - original state
         # nst - next state
         # a - the index to the action taken
         # r - reward obtained
         def learn(self,ost,nst,a,r):
-                # define this function
                 #print("learn something from this data")
+
+                old_q = self.q_table[ost][a]
+                next_max = max(self.q_table[nst] or [0]) #Prevents empty list case
+        
+                new_q = (1 - alpha) * old_q + alpha * (r + gamma * next_max)
+                self.q_table[ost][a] = new_q
                 
                 return
+
+        
